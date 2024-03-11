4b7b4bcb0aac80d3150cb854b7b4e02647b1bd13
adamhung60 <120136367+adamhung60@users.noreply.github.com>
2024-03-10 20:25:56 -0400

    Update agent.py

--- agent.py
+++ agent.py
@@ -16,27 +16,18 @@ def train(ts):
                               norm_reward=True, 
                               clip_obs=1000.0, 
                               clip_reward=1000.0, 
-                              #gamma=0.99, 
-                              #epsilon=1e-08, 
-                              #norm_obs_keys=None
                               )
-    callback_on_best = StopTrainingOnRewardThreshold(reward_threshold = 81)                                         
+    callback_on_best = StopTrainingOnRewardThreshold(reward_threshold = 11)                                         
     eval_callback = EvalCallback(vec_env,
                              log_path="logs/", 
-                             #best_model_save_path="logs/",
-                             eval_freq=int(5e5),
+                             eval_freq=int(1e6),
                              n_eval_episodes= 1000,
                              deterministic=True, 
                              callback_on_new_best=callback_on_best)
-    
-    #policy_kwargs = dict(net_arch=[16,16])
 
     model = PPO("MlpPolicy", vec_env, verbose=0,
-                #n_epochs = 16,
-                #learning_rate=  0.0002,
-                #n_steps= 8192,
-                #policy_kwargs=policy_kwargs
-                #ent_coef= 0.01,
+                gamma = 1.0,
+                n_steps= 8192,
                 ) 
 
     model.learn(total_timesteps=ts,progress_bar=True, callback=eval_callback)
@@ -49,15 +40,13 @@ def test(eps):
     test_env = gym.make("TT2", render_mode="human")
     test_env = Monitor(test_env)
     vec_test_env = DummyVecEnv([lambda: test_env])
-    vec_test_env = VecNormalize.load("logs/vec_normalize.pkl", vec_test_env)
+    vec_test_env = VecNormalize.load("logs/vec_normalize_bob.pkl", vec_test_env)
 
-    #model = PPO.load("logs/best_model")
-    model = PPO.load("logs/ppo_tt")
+    model = PPO.load("logs/ppo_bob")
     
     mean_rewards, _ = evaluate_policy(model, vec_test_env, n_eval_episodes=eps, deterministic=True, return_episode_rewards=True)
-    print("Rewards:", mean_rewards)
-
+    #print("Rewards:", mean_rewards)
         
 if __name__ == '__main__':
-    train(5e7)
-    test(20)
+    train(2e7)
+    test(1000)
